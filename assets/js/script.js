3d49848fba36b3f97823294315515a821e9d1c83
Arcade Kakpo <arcadej@hotmail.fr>
2024-07-30 22:43:33 +0000

    Add FormData and check if the entries are logged

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
-const API_KEY = "jDHZZaCLttQpDgBcwUWayhGUzpw1";
+const API_KEY = "jDHZZaCLttQpDgBcwUWayhGUzpw";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    for (let e of form.entries()) {
+        console.log(e);
+    }
+}
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -12,10 +21,21 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data.expiry);
-    }
-    else {
+        displayStatus(data);
+    } else {
         throw new Error(data.error);
     }
+}
 
+function displayStatus(data) {
+
+    let heading = "API Key Status";
+    let results1 = `Your key is valid until`;
+    let results2 =`${data.expiry}`;
+    
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results1-content").innerText = results1;
+    document.getElementById("results2-content").innerText = results2;
+    
+    resultsModal.show();
 }
